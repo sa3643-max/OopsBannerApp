cb3efdf28f12869d0491962811eb60a7e173c547
sa3643 <sa3643@srmist.edu.in>
2026-03-03 18:23:50 +0530

    Added UC6

--- OopsBannerApp.java
+++ OopsBannerApp.java
@@ -1,20 +1,61 @@
 public class OopsBannerApp {
- public static void main(String[] args) {
+public static void main(String[] args) {
 
-        // Combine array declaration + initialization using String.join()
-        String[] banner = {
-                String.join("  ", " ***** ", " ***** ", "****** ", " ***** "),
-                String.join("  ", "*     *", "*     *", "*     *", "*     *"),
-                String.join("  ", "*     *", "*     *", "*     *", "*      "),
-                String.join("  ", "*     *", "*     *", "****** ", " ***** "),
-                String.join("  ", "*     *", "*     *", "*      ", "      *"),
-                String.join("  ", "*     *", "*     *", "*      ", "*     *"),
-                String.join("  ", " ***** ", " ***** ", "*      ", " ***** ")
-        };
+        // Build the banner lines using static methods
+        String[] banner = new String[7];
 
-        // Enhanced for-loop to print each line
+        for (int i = 0; i < 7; i++) {
+            // Combine letters for each row
+            banner[i] = String.join("  ",
+                    getO()[i],   // First O
+                    getO()[i],   // Second O
+                    getP()[i],   // P
+                    getS()[i]    // S
+            );
+        }
+
+        // Print the final banner
         for (String line : banner) {
             System.out.println(line);
         }
+    }
+
+    // Static method to generate letter O
+    public static String[] getO() {
+        return new String[] {
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+    }
+
+    // Static method to generate letter P
+    public static String[] getP() {
+        return new String[] {
+                "****** ",
+                "*     *",
+                "*     *",
+                "****** ",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+    }
+
+    // Static method to generate letter S
+    public static String[] getS() {
+        return new String[] {
+                " ***** ",
+                "*     *",
+                "*      ",
+                " ***** ",
+                "      *",
+                "*     *",
+                " ***** "
+        };
     }
 }
