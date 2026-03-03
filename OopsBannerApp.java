f9d2df36fc6c2d4d69fb4e955a25bfb16af6e72e
sa3643 <sa3643@srmist.edu.in>
2026-03-03 17:50:38 +0530

    Added UC1

--- OopsBannerApp.java
+++ OopsBannerApp.java
@@ -2,12 +2,13 @@ public class OopsBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(" *****     *****    *****    ***** ");
-        System.out.println("*     *   *     *   *     *  *     ");
-        System.out.println("*     *   *     *   *     *  *     ");
-        System.out.println("*     *   *     *   *****    ***** ");
-        System.out.println("*     *   *     *   *        *     ");
-        System.out.println("*     *   *     *   *        *     ");
-        System.out.println(" *****     *****    *        ***** ");
+        System.out.println(" *****    *****    ******    ***** ");
+        System.out.println("*     *  *     *  *     *  *     *");
+        System.out.println("*     *  *     *  *     *  *      ");
+        System.out.println("*     *  *     *  ******    ***** ");
+        System.out.println("*     *  *     *  *              *");
+        System.out.println("*     *  *     *  *        *     *");
+        System.out.println(" *****    *****   *         ***** ");
+
     }
-} 
+}
